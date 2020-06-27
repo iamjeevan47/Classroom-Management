1ac1586dd1dd265d5c37435827b8f5f4fd03fd14
Jeevandeep Saini <jeevandeepsaini@gmail.com>
2020-06-27 08:27:01 +0530

    commit

--- app/src/main/java/com/example/classroommanagement/Category.java
+++ app/src/main/java/com/example/classroommanagement/Category.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class demo extends AppCompatActivity {
+public class Category extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_demo);
+        setContentView(R.layout.activity_category);
     }
 }
