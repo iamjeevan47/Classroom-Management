0695ff7b289e7720c0b3cf830e2c484364a1143a
Jeevandeep Saini <jeevandeepsaini@gmail.com>
2020-06-28 14:08:18 +0530

    commit

---
+++ app/src/main/java/com/example/classroommanagement/addteacher.java
@@ -0,0 +1,14 @@
+package com.example.classroommanagement;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class addteacher extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_addteacher);
+    }
+}
